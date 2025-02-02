6cf569ed3df525cb50edd476cd2a4e75a8711f2e
abduvali0626 <umarrrrrrrrrrrrrrr63@gmail.com>
2025-02-02 09:17:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let rasm = document.querySelector('.image') 
+
+
+function ochirish() {
+    rasm.style.filter = 'grayscale(1)  brightness(0.1)';
+}
+
+function yoqilish() {
+  rasm.style.filter = "grayscale(0)  brightness(1)";
+}
